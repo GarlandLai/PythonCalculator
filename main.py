ee66b44e1ca61d5d0149e08a5f2cf520a7028f8e
Garland Lai <garlandlai24@gmail.com>
2020-08-27 18:18:52 -0700

    use built in eval to do math

--- main.py
+++ main.py
@@ -7,14 +7,26 @@ print("Type 'quit' to exit\n")
 previous = 0
 run = True
 
-def performMath():
+
+def perform_math():
     global run
-    equation = input("Enter equation:")
+    global previous
+    if previous == 0:
+        equation = input("Enter equation:")
+    else:
+        equation = input(str(previous))
     if equation == "quit":
+        print("Thanks for using my calculator")
         run = False
     else:
-        print("You typed: ", equation)
+        # Need some regex to remove incorrect typed inputs
+        equation = re.sub('[a-zA-Z,.:()" "]', '', equation)
+
+        if previous == 0:
+            previous = eval(equation)
+        else:
+            previous = eval(str(previous) + equation)
 
 
 while run:
-    performMath()
+    perform_math()
